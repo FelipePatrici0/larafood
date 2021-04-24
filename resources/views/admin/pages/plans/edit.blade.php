9b544896ee6e972da29e4a2d9a4b5375292092c8
FelipePatrici0 <felipepatricio38@gmail.com>
2021-04-24 17:57:09 -0300

    gestão de planos

--- resources/views/admin/pages/plans/edit.blade.php
+++ resources/views/admin/pages/plans/edit.blade.php
@@ -1,19 +1,19 @@
 @extends('adminlte::page')
 
-@section('title', 'Cadastrar Novo Planos')
+@section('title', 'Editar plano')
 
 @section('content_header')
-    <h1>Cadastrar Novo Planos </h1>
+    <h1>Editar plano - {{ $plan->name }}</h1>
 @stop
 
 @section('content')
     <div class="card">
        <div class="card-body">
-           <form action="{{ route('plans.store') }}" class="form" method="POST">
+           <form action="{{ route('plans.update', $plan->url) }}" class="form" method="POST">
                @csrf
+               @method('PUT')
                @include('admin.pages.plans._partials.form')
            </form>
        </div>
     </div>
 @stop
-
